e1dd1eda7da19a3750b1a36873381f15d464b7a5
Justin Johnson <threejay20@yahoo.ca>
2023-05-10 19:35:20 -0400

    Dashboard and Navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,29 +4,28 @@ import { ThemeProvider, createTheme } from "@mui/material/styles"
 import { Box, CssBaseline } from "@mui/material"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
+
 
 function App() {
-
-  const theme = useMemo(() => createTheme(themeSettings), [])
-
-
-  return  <div className="app">
- <BrowserRouter>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<div>dashboard page</div>} />
-          <Route path="/predictions" element={<div> prediction page</div>} />
-        </Routes>
-        </Box>
-
-    </ThemeProvider>
-    </BrowserRouter>
-  </div>
-       
-  
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route 
+              path="/predictions" element={<div>predictions</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
